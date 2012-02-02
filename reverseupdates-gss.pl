f0e56330fcc513acdc6baae97394cc9d4328ee4b
Ismael Lezcano <Ismael.Lezcano@gmail.com>
2012-02-02 15:59:26 -0500

    Made changes to reverse update script, and new one that switches to gss updates

--- reverseupdates-gss.pl
+++ reverseupdates-gss.pl
@@ -7,6 +7,7 @@ use strict;
 
 $\ = "\n";
 my $reftoany = ["any"];  # That's right. Just a ref to the array with the array scalar. Just so I don't keep defining it.
+my $reftonull = [];  # That's right. Just a ref to the array to nothing. Just so I don't keep defining it.
 my $bloxmaster = 'ryentp2.rye.avon.com';
 my $creds = Net::Netrc->lookup($bloxmaster);
 my $session = Infoblox::Session->new("master"=> $bloxmaster, "username"=>$creds->login, "password"=>$creds->password);
@@ -24,10 +25,18 @@ foreach (@zones)
 	{
 	next if ($_->name eq '127.0.0.0/24');   # Gotta skip loopback
 
-	unless (@{$_->allow_update()} eq @$reftoany)
+	unless ($_->allow_gss_tsig_zone_updates() eq 'true')
+		{
+		print $_->name . " needs to have TSIG enabled.";
+		$_->allow_gss_tsig_zone_updates('true');
+		my $result = $session->modify($_);
+		if ($result) {print "Succesfully changed " . $_->name;}
+			else {warn $session->status_detail()}
+		}
+	if (@{$_->allow_update()})
 		{
 		print $_->name . " needs changing.";
-		$_->allow_update($reftoany);
+		$_->allow_update($reftonull);
 		my $result = $session->modify($_);
 		if ($result) {print "Succesfully changed " . $_->name;}
 			else {warn $session->status_detail()}
