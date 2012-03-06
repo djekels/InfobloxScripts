808d262cee1497b1398e05cba6fa4565650519b0
Ismael <ish@dorcas.lezcano.org>
2012-03-06 15:49:38 -0500

    Added a script to get the name of an address

---
+++ getaddress.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl -w
+#
+use Infoblox;
+use Net::Netrc;
+use Getopt::Std;
+use XML::Dumper;
+use strict;
+
+
+my $bloxmaster = 'dns1.avon.com';
+my $creds = Net::Netrc->lookup($bloxmaster);
+my %options;
+getopts("a: ", \%options);
+my $address = $options{a} or die;
+my $session = Infoblox::Session->new("master"=> $bloxmaster, "username"=>$creds->login, "password"=>$creds->password);
+
+unless ($session) {
+               die("Construct session failed: ",
+                       Infoblox::status_code() . ":" . Infoblox::status_detail());
+                }
+
+my $ipamobject = $session->get(object=>'Infoblox::IPAM::Address',
+	address=>$address,
+	);
+
+if ($ipamobject)
+	{
+	local $, = "\n\t";
+	print "$address has the following names:\n\t";
+	print @{$ipamobject->names};
+	print "\n";
+	}
+	else {print "No address $address\n"}
