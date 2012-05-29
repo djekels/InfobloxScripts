8aa7224c46f8b4bc10f1c2511072d1cb50880263
Ismael <ish@dorcas.lezcano.org>
2012-05-29 08:01:40 -0400

    Some Changes to NetscalerServices script

--- netscalerservices.pl
+++ netscalerservices.pl
@@ -3,64 +3,26 @@
 # I wrote this to add an alias to a host record
 #
 use Infoblox;
-use Getopt::Std;
 use Net::Netrc;
 use XML::Dumper;
 use strict;
 
-my @countryprefixes = (
-'al',
-'ar',
-'au',
-'ba',
-'bg',
-'br',
-'ca',
-'cl',
-'cn',
-'co',
-'cz',
-'de',
-'do',
-'ee',
-'fi',
-'ge',
-'hr',
-'hu',
-'ie',
-'in',
-'it',
-'jp',
-'kg',
-'kr',
-'kz',
-'lt',
-'lv',
-'md',
-'mk',
-'mx',
-'my',
-'nz',
-'ph',
-'pl',
-'pr',
-'pt',
-'ro',
-'rs',
-'ru',
-'si',
-'sk',
-'th',
-'tr',
-'tw',
-);
+my @countryprefixes = qw/
+za
+jt
+cy
+lb
+mt
+mu
+ct
+om
+ae
+is
+/;
 
 my %hostorder = ('ryelxwebeqw1.avon.net'=>3, 'ryelxwebeqw2.avon.net'=>4);
 my @hosts = (keys %hostorder);
-$Getopt::Std::STANDARD_HELP_VERSION = 1;
-my %options;
-getopts("m: ", \%options);
-my $bloxmaster = $options{m};
+my $bloxmaster = 'ryeinfoblox.global.avon.com';
 my $creds = Net::Netrc->lookup($bloxmaster);
 my $session = Infoblox::Session->new("master"=> $bloxmaster, "username"=>$creds->login, "password"=>$creds->password);
 
