3b68711f4c2abab20226f9214fd2d785356ff724
Ismael Lezcano <Ismael.Lezcano@gmail.com>
2012-07-06 13:45:40 -0400

    Added script to add CNAME object

---
+++ addcname.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl -w
+
+use Infoblox;
+use Net::Netrc;
+use Getopt::Std;
+#use XML::Dumper;
+use strict;
+
+my $bloxmaster = 'ryeinfoblox.global.avon.com';
+my $creds = Net::Netrc->lookup($bloxmaster);
+my $session = Infoblox::Session->new("master"=> $bloxmaster, "username"=>$creds->login, "password"=>$creds->password);
+
+my %options;
+getopts("a:t: ", \%options);
+
+my $alias = $options{a} or die;
+my $target = $options{t} or die;
+my $cname = Infoblox::DNS::Record::CNAME->new(
+	'canonical'=>$target,
+	'name'=>$alias,
+	);
+
+$session->add($cname) || die $session->status_detail();
