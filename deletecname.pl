c42e2db8cb0a2a685a2555d78e7bc2b3bdca2892
Ismael Lezcano <Ismael.Lezcano@gmail.com>
2012-07-09 15:54:14 -0400

    Added a CNAME deletion script.

---
+++ deletecname.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+use Infoblox;
+use Net::Netrc;
+#use XML::Dumper;
+use strict;
+
+my $cname = shift || die 'No CNAME specified.';
+my $bloxmaster = 'ryeinfoblox.global.avon.com';
+my $creds = Net::Netrc->lookup($bloxmaster);
+my $session = Infoblox::Session->new("master"=> $bloxmaster, "username"=>$creds->login, "password"=>$creds->password);
+
+unless ($session)
+	{	
+        die("Construct session failed: ",
+           Infoblox::status_code() . ":" . Infoblox::status_detail());
+	}
+
+my $targetcname = $session->get ( object=> 'Infoblox::DNS::Record::CNAME',
+                        name=>$cname,
+                        );
+
+local $\ = "\n";
+if ($targetcname)
+	{
+	local $, = "\n";
+	print "Target host is " . $targetcname->name;
+
+	my $result = $session->remove($targetcname);
+
+	if ($result) {print "Successfully deleted alias."} else {print $session->status_detail()}
+	}
+else
+	{
+	print "No applicable cname.";
+	}
