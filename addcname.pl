e81738e6848fd5e0da30848d487a57f73e0e9859
Ismael Lezcano <Ismael.Lezcano@gmail.com>
2012-07-09 16:02:52 -0400

    Adjustments to AddCname so that I can add comments.

--- addcname.pl
+++ addcname.pl
@@ -11,7 +11,7 @@ my $creds = Net::Netrc->lookup($bloxmaster);
 my $session = Infoblox::Session->new("master"=> $bloxmaster, "username"=>$creds->login, "password"=>$creds->password);
 
 my %options;
-getopts("a:t: ", \%options);
+getopts("a:t:c: ", \%options);
 
 my $alias = $options{a} or die;
 my $target = $options{t} or die;
@@ -19,5 +19,7 @@ my $cname = Infoblox::DNS::Record::CNAME->new(
 	'canonical'=>$target,
 	'name'=>$alias,
 	);
+
+$cname->comment($options{c}) if ($options{c});
 
 $session->add($cname) || die $session->status_detail();
